d4756051a75e996d0f10ed882539a34d3a6d67e4
KIMCHANYEOL <123149441+KIMCHANYEOL@users.noreply.github.com>
2023-02-01 17:28:37 +0900

    Update Kcy06.py

--- Kcy06.py
+++ Kcy06.py
@@ -103,8 +103,7 @@ with st.form(key ='Form1'):
         
         
 if select_language =='Money_rates and House prices':
-    try:
-          plotting_demo()  
+    plotting_demo()  
 
 elif select_language =='KBO_Ranking and Winning rates':
     bar_chart()
