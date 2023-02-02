8e58a975213fb74c9f3eb421f9dd438d57d49f4b
KIMCHANYEOL <123149441+KIMCHANYEOL@users.noreply.github.com>
2023-02-02 16:34:38 +0900

    Update Kcy06.py

--- Kcy06.py
+++ Kcy06.py
@@ -115,7 +115,7 @@ if select_language =='Money_rates and House_prices':
         tab1.subheader ("A tap with a chart")
         plotting_demo()
         
-    with tap2:
+    with tab2:
         tab2.subheader ("A tab with the data")
         st.dataframe(aa)
         
